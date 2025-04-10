a4a9d9eae96afad6906e2332a9fa213e438f263c
Edelwy <nina.mislej1@gmail.com>
2025-04-11 00:02:44 +0200

    implement two less

--- queens.py
+++ queens.py
@@ -1,28 +1,31 @@
 from pysat.formula import CNF
 from pysat.solvers import Glucose3
 
-PRINT_CNF = 0
-PRINT_SOLUTION = 1
+PRINT_CNF = 0 # Print the CNF obtained.
+PRINT_SOLUTION = 1 # Print the solution obtained.
+LINEAR_VARIABLES = 1 # Use linear variables vs. concated variables.
 
-# Variables are presented as xij where row is the row and column the column.
+# Variables are presented as (i,j) where i is the row and j the column.
 # Since this cannot be the case with CNF we use a function to represent it linearly. 
 def lin(n: int, row: int, column: int) -> int:
-    return n*row + column + 1
+    if LINEAR_VARIABLES:
+        return n*row + column + 1
+    return int(f"{row}{column}")
 
 def valid(n: int, row: int, column: int) -> bool:
     if row < 0 or row >= n or column < 0 or column >= n:
         return False
     return True
 
-def one_per_clause(n: int, cnf: CNF, clauses: list) -> None:
-    for row, clause_1 in enumerate(clauses):
-        for clause_2 in clauses[row + 1:]:
-            cnf.append([-clause_1, -clause_2])
+def one_per_clause(cnf: CNF, clauses: list) -> None:
+    for row, clause1 in enumerate(clauses):
+        for clause2 in clauses[row + 1:]:
+            cnf.append([-clause1, -clause2])
 
 def generate_CNF(n: int) -> CNF:
     cnf = CNF()
 
-    # There can be only one queen per row and column.
+    # There can be exactly one queen per row and column.
     for row in range(n):
         row_constraints = []
         column_constraints = []
@@ -31,10 +34,10 @@ def generate_CNF(n: int) -> CNF:
             column_constraints.append(lin(n, column, row))
         cnf.append(row_constraints) 
         cnf.append(column_constraints) 
-        one_per_clause(n, cnf, row_constraints) 
-        one_per_clause(n, cnf, column_constraints)
+        one_per_clause(cnf, row_constraints) 
+        one_per_clause(cnf, column_constraints)
             
-    # There can only be one queen per diagonal.
+    # There can be at most one queen per diagonal.
     for offset in range(n):
         upper_diag, lower_diag, upper_antidiag, lower_antidiag = [[] for _ in range(4)]
         for diag, antidiag in zip(range(n), range(n - 1, -1, -1)):
@@ -46,18 +49,24 @@ def generate_CNF(n: int) -> CNF:
                 upper_antidiag.append(lin(n, diag, antidiag + offset))
             if valid(n, antidiag, antidiag - offset):
                 lower_antidiag.append(lin(n, diag, antidiag - offset))
-        one_per_clause(n, cnf, upper_diag)
-        one_per_clause(n, cnf, lower_diag)
-        one_per_clause(n, cnf, upper_antidiag)
-        one_per_clause(n, cnf, lower_antidiag)
+        one_per_clause(cnf, upper_diag)
+        one_per_clause(cnf, lower_diag)
+        one_per_clause(cnf, upper_antidiag)
+        one_per_clause(cnf, lower_antidiag)
         
     return cnf
 
+def get_solution(n):
+    cnf = generate_CNF(n)
+    if PRINT_CNF: 
+        print(cnf.to_dimacs())
+    solver = Glucose3()
+    solver.append_formula(cnf)
+    solver.solve()
+    return solver.get_model()
+
 if __name__ == "__main__":
-    cnf = generate_CNF(4)
-    if PRINT_CNF: print(cnf.to_dimacs())
+    chessboard_size = 4
+    solution = get_solution(chessboard_size)
     if PRINT_SOLUTION: 
-        solver = Glucose3()
-        solver.append_formula(cnf)
-        print(f"Solvable: {solver.get_model()}") if solver.solve() else print("Unslovable")
-        solver.delete()
+        print(f"Solvable: {solution}") if solution else print("Unslovable")
--- two_less.py
+++ two_less.py
@@ -1,18 +1,112 @@
 from pysat.formula import CNF
 from pysat.solvers import Glucose3
 
-PRINT_CNF = 0
-PRINT_SOLUTION = 1
+PRINT_CNF = 0 # Print the CNF obtained.
+PRINT_SOLUTION = 0 # Print the solution obtained.
+PRINT_TRANSLATION = 1 # Print the translated solution to readable output.
+LINEAR_VARIABLES = 1 # Use linear variables vs. concated variables.
 
-def generate_CNF(n: int) -> CNF:
+# Variables are presented as (i,a,b,c) where (a,b,c) is the touple and i is the position in the chain.
+# Since this cannot be the case with CNF we use a function to represent it linearly. 
+# To ensure uniquenes we evalute the polynomial ix^3 + ax^2 + bc + c at n.
+def lin(n: int, a: int, b: int, c: int, i: int) -> int:
+    if LINEAR_VARIABLES:
+        return i * n**3 + a * n**2 + b * n + c + 1  
+    return int(f"{a}{b}{c}{i}")
+
+def decode(n, var):
+    var = var - 1
+    c = var % n
+    var = var // n
+    b = var % n
+    var = var // n
+    a = var % n
+    var = var // n
+    i = var
+    return (a, b, c, i)
+
+def translate(n: int, solution: list[int] ) -> dict[tuple]:
+    translation = {}
+    for var in solution:
+        if var > 0:
+            a, b, c, i = decode(n, var)
+            translation[i] = (a, b, c)
+    return translation
+
+# Returns true if tuple 2 is greater than tuple 1.
+def greater(tuple1: tuple, tuple2: tuple):
+    a, b, c = tuple1
+    x, y, z = tuple2
+    if x > a and y > b:
+        return True
+    if x > a and z > c:
+        return True
+    if y > b and z > c:
+        return True
+    return False
+
+# Constraints if tuple 2 is not greater than tuple 1.
+def not_consecutive(cnf: CNF, n: int, k: int, tuple1: tuple, tuple2: tuple) -> list:
+    for i in range(k - 1):
+        cnf.append([-lin(n, *tuple1, i), -lin(n, *tuple2, i + 1)])
+
+def one_per_clause(cnf: CNF, clauses: list) -> None:
+    for row, clause1 in enumerate(clauses):
+        for clause2 in clauses[row + 1:]:
+            cnf.append([-clause1, -clause2])
+
+def generate_CNF(n: int, k: int) -> CNF:
     cnf = CNF()
+
+    # Generate all tuples only once.
+    tuples = []
+    for a in range(n):
+        for b in range(n):
+            for c in range(n):
+                three_tuple = (a, b, c)
+                tuples.append(three_tuple)
+
+    # There can be exactly one tuple per position in chain.
+    for i in range(k):
+        chain_constraint = []
+        for three_tuple in tuples:
+            chain_constraint.append(lin(n, *three_tuple, i))
+        cnf.append(chain_constraint)
+        one_per_clause(cnf, chain_constraint)
+
+    # One tuple can be in at most one position.
+    for three_tuple in tuples:
+        tuple_constraint = []
+        for i in range(k):
+            tuple_constraint.append(lin(n, *three_tuple, i))
+        one_per_clause(cnf, tuple_constraint)
+
+    # If tuple B is not greater than tuple A then we cannot have both (A,i) and (B,i+1) be true.
+    # First element of the pair is the i-th position and the second element the i+1-th position.
+    for tuple1 in tuples:
+        for tuple2 in tuples:
+            if not greater(tuple1, tuple2):
+                not_consecutive(cnf, n, k, tuple1, tuple2)
     return cnf
 
+def find_max(n):
+    for k in range(n**3 - 1, -1, -1):
+        solution = get_solution(n, k)
+        if not solution:
+            continue
+        print(f"Solution: {translate(n, solution)}")
+        break
+
+def get_solution(n, k):
+    cnf = generate_CNF(n, k)
+    if PRINT_CNF: 
+        print(cnf.to_dimacs())
+    solver = Glucose3()
+    solver.append_formula(cnf)
+    solver.solve()
+    return solver.get_model()
+
 if __name__ == "__main__":
-    cnf = generate_CNF(4)
-    if PRINT_CNF: print(cnf.to_dimacs())
-    if PRINT_SOLUTION: 
-        solver = Glucose3()
-        solver.append_formula(cnf)
-        print(f"Solvable: {solver.get_model()}") if solver.solve() else print("Unslovable")
-        solver.delete()
+    n = 3
+    find_max(n)
+    
